f763affaa9e326165b2161f6b56fb644896416e8
Stefan Penner <stefan.penner@gmail.com>
2016-03-02 22:03:51 -0800

    [BUGFIX] always normalize paths to posix (not windows \)

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var throwOnRootAccess = true;
+var ensurePosix = require('ensure-posix-path');
 
 function resolveModules(options) {
   options = options || {};
-  
+
   if (options.throwOnRootAccess === false) {
     throwOnRootAccess = false;
   }
-  
+
   return moduleResolve;
 }
 
-function moduleResolve(child, name) {
+function moduleResolve(_child, _name) {
+  var child = ensurePosix(_child);
+  var name = ensurePosix(_name);
   if (child.charAt(0) !== '.') { return child; }
 
   var parts = child.split('/');
@@ -30,7 +33,6 @@ function moduleResolve(child, name) {
       }
       parentBase.pop();
     } else if (part === '.') {
-      
       continue;
     } else { parentBase.push(part); }
   }
